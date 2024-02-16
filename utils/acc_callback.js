45c38ad9975909040fc523c9622b3d4ab0798af6
Kent Adrian Sato <adriankentsato@gmail.com>
2024-02-16 15:36:10 +0800

    CLEAN: Semicolon

--- utils/acc_callback.js
+++ utils/acc_callback.js
@@ -9,8 +9,8 @@
  */
 
 
-const Q = require('fastq');
-const uuid = require('uuid').v4;
+const Q = require('fastq')
+const uuid = require('uuid').v4
 
 /**
  * Accumulate data from callbacks, just like Promise.all
@@ -19,59 +19,59 @@ const uuid = require('uuid').v4;
  * @returns 
  */
 const acc_callback = ( num, cb ) => {
-    const data = [];
+    const data = []
 
     const queue = new Q(( { obj, cmd }, xb ) => {
         switch( cmd ) {
             case 'reset':
                 // remove all data
-                data.length = 0;
-                xb();
-                break;
+                data.length = 0
+                xb()
+                break
             default:
-                data.push( obj );
-
-                xb(); // This is needed to execute the next item in queue
+                data.push( obj )
+                
+                // This is needed to execute the next item in queue
+                xb() 
 
                 // Check if the length satisfies the number of accumated data to return
                 if ( data.length === num ) {
                     // Check for errors added into the list.
-                    const errors = data.filter( ( d ) => d instanceof Error );
+                    const errors = data.filter( ( d ) => d instanceof Error )
 
                     // If no errors return all data.
                     if ( errors.length === 0 ) {
-                        cb( null, data );
+                        cb( null, data )
                     } else {
                         // if there's an error just return the first one.
-                        cb( errors[0] );
+                        cb( errors[0] )
                     }
                 }
-                break;
+                break
         }
-    }, 1);
+    }, 1)
 
-    let ctr = 0;
+    let ctr = 0
 
     return {
         accumulate: ( obj ) => {
-            ++ctr;
+            ++ctr
 
             if ( ctr > num ) {
-                ctr = num;
-                throw new Error('Cannot call this anymore, please call reset.');
+                ctr = num
+                throw new Error('Cannot call this anymore, please call reset.')
             }
 
-            queue.push({ id: uuid(), obj });
+            queue.push({ id: uuid(), obj })
         },
         /**
          * Resets the accumulator, also resets the previous results' references.
          */
         reset: () => {
-            ctr = 0;
+            ctr = 0
 
-            queue.push({ id: uuid(), obj: null, cmd: 'reset' });
+            queue.push({ id: uuid(), obj: null, cmd: 'reset' })
         },
-    };
-};
-
-module.exports.acc_callback = acc_callback;
+    }
+}
+module.exports.acc_callback = acc_callback
--- utils/password.js
+++ utils/password.js
@@ -3,7 +3,7 @@
  * Date: 10/21/2023
  */
 
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcrypt')
 
 /**
  * Hash password
@@ -13,19 +13,19 @@ const bcrypt = require('bcrypt');
 const hash_password = ( pass, cb ) => {
     bcrypt.genSalt(10 , ( err, salt ) => {
         if ( err || !salt ) {
-            cb( new Error( 'Cannot hash password.' ) );
+            cb( new Error( 'Cannot hash password.' ) )
         } else {
             bcrypt.hash(pass, salt, ( err, hashed ) => {
                 if ( err || !hashed ) {
-                    cb( new Error( 'Cannot hash password.') );
+                    cb( new Error( 'Cannot hash password.') )
                 } else {
-                    cb( null, hashed.toString() );
+                    cb( null, hashed.toString() )
                 }
-            });
+            })
         }
-    });
-};
-module.exports.hash_password = hash_password;
+    })
+}
+module.exports.hash_password = hash_password
 
 /**
  * Compare password
@@ -36,10 +36,10 @@ module.exports.hash_password = hash_password;
 const compare_password = ( pass_str, pass_hash, cb ) => {
     bcrypt.compare(pass_str, pass_hash, ( err, same ) => {
         if ( err ) {
-            cb( new Error( 'Passwords not the same.' ) );
+            cb( new Error( 'Passwords not the same.' ) )
         } else {
-            cb( null, same );
+            cb( null, same )
         }
-    });
-};
-module.exports.compare_password = compare_password;
+    })
+}
+module.exports.compare_password = compare_password
